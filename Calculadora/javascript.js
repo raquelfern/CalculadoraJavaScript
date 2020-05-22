30374f9be07d65d2c4435e856ca94c826e333d18
raquelfern <61147761+raquelfern@users.noreply.github.com>
2020-05-22 01:19:07 -0300

    Add files via upload

---
+++ Calculadora/javascript.js
@@ -0,0 +1,15 @@
+function preencher(valor){
+    document.getElementById('visor').value += valor;
+}
+
+function calcular(){
+    var resultado = 0;
+    resultado = document.getElementById('visor').value;
+    document.getElementById('visor').value = ' ';
+    document.getElementById('visor').value = eval(resultado);
+    //alert(resultado);
+}
+
+function limpar(){
+    document.getElementById('visor').value = ' ';
+}
